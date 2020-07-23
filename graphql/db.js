0018bb9a5bcf820925f310990db9e5de92e60ac6
Yuna Lee <carnivalss@naver.com>
2020-07-24 04:27:40 +0900

    return data

--- graphql/db.js
+++ graphql/db.js
@@ -2,7 +2,7 @@ import fetch from "node-fetch";
 const API_URL = "https://yts.am/api/v2/list_movies.json";
 
 export const getMovies = (limit, rating) => {
-	fetch(`${API_URL}`)
+	return fetch(`${API_URL}`)
 		.then(res => res.json())
 		.then(json => json.data.movies);
 };
