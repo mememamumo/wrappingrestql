37616c5837e358002da35e217d3315321f22550d
Yuna Lee <carnivalss@naver.com>
2020-07-30 18:58:37 +0900

    PORT

--- index.js
+++ index.js
@@ -4,7 +4,13 @@ import resolvers from "./graphql/resolvers";
 
 const server = new GraphQLServer({
 	typeDefs: "graphql/schema.graphql",
-	resolvers
+	resolvers,
+	introspections: true,
+	playground: true
 });
 
-server.start({ port: 7000 },() => console.log("🤡Graphql Server Running"));
+// server.start({ port: 7000 },() => console.log("🤡Graphql Server Running"));
+
+server.listen({ port: process.env.PORT || 7000 }).then(({ url }) => {
+  console.log(`🚀 Server ready at ${url}`);
+});
